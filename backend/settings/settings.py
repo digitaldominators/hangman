e06a39ceadd3f7c57a5203070af3b40a6478d2c9
Shlomo Markowitz <75799735+smark-1@users.noreply.github.com>
2024-01-12 14:15:45 -0500

    added django rest framework

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# rest frameword settings
+REST_FRAMEWORK = {
+}
--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))]

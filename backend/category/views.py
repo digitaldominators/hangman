1bc5f0dbf187c15389d0472f70f1187153926d73
Shlomo Markowitz <75799735+smark-1@users.noreply.github.com>
2024-02-06 21:29:47 -0500

    added a minimum number of phrases for the category to show up

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Q
 from rest_framework import viewsets
 from rest_framework.response import Response
 
@@ -7,8 +8,9 @@ from .serializers import CategorySerializer
 
 # Create your views here.
 class CategoryViewSet(viewsets.GenericViewSet):
-    queryset = Category.objects.filter(active=True)
-
+    def get_queryset(self):
+        queryset = Category.objects.filter(active=True).annotate(phrase_count=Count('phrase')).filter(phrase_count__gte=20)
+        return queryset
     def list(self, request, *args, **kwargs):
-        serializer = CategorySerializer(self.queryset, many=True)
+        serializer = CategorySerializer(self.get_queryset(), many=True)
         return Response(serializer.data)

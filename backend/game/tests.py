10d781302a08ebd104aeea8e95cb6037dda10a3d
Shlomo Markowitz <75799735+smark-1@users.noreply.github.com>
2024-02-13 19:50:35 -0500

    added test test

--- backend/game/tests.py
+++ backend/game/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_test(self):
+        """Test that django test run in github actions"""
+        self.assertEqual('The lion says "roar"', 'The lion says "roar"')

3baf9a42dd2d9d20143c1122ff7c08b9fb4b4c27
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-02-19 14:57:54 -0500

    redirect logged-out users to login page when going to accounts page (#36)
    * redirect logged-out users to login page when going to accounts page
    
    * Automated formatting with black/prettier

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.http import Http404
 from django.template.exceptions import TemplateDoesNotExist
 from django.urls import path, include
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import routers
 
 from game.views import GameViewSet
@@ -30,30 +31,37 @@ from category.views import CategoryViewSet
 # from accounts.views import AccountViewSet
 
 router = routers.DefaultRouter()
-router.register(r'game', GameViewSet, basename='game')
-router.register(r'settings', DefaultSettingsViewSet, basename='settings')
-router.register(r'scoreboard', ScoreboardViewSet, basename='scoreboard')
-router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r"game", GameViewSet, basename="game")
+router.register(r"settings", DefaultSettingsViewSet, basename="settings")
+router.register(r"scoreboard", ScoreboardViewSet, basename="scoreboard")
+router.register(r"categories", CategoryViewSet, basename="categories")
 
 # router.register(r'accounts', AccountViewSet, basename='account')
 
+
 def game_view(request, slug=None):
-    template_name = 'index.html'
+    template_name = "index.html"
     if slug:
         template_name = slug + ".html"
+        if template_name == "account.html":
+            if not request.user.is_authenticated:
+                return redirect("/login/")
     try:
         return render(request, template_name)
     except TemplateDoesNotExist:
         raise Http404()
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', game_view),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", game_view),
     path("<slug:slug>/", game_view),
 ]
 
 if settings.DEBUG:
-    urlpatterns += [path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))]
+    urlpatterns += [
+        path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
+    ]

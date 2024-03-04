7a6241e6ce0cabb72d2ee9a8239764d82e82109d
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-03-04 01:18:30 +0000

    Automated formatting with black/prettier

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -6,7 +6,7 @@ import {
   draw_next_body_part,
   refreshCanvas,
 } from "./stageCanvas.js";
-import readCookie from './readCookie.js'
+import readCookie from "./readCookie.js";
 import axios from "axios";
 import barba from "@barba/core";
 let body_type_index = 0;
@@ -14,7 +14,7 @@ function next_part() {
   if (get_current_part_index() >= get_total_parts()) {
     body_type_index += 1;
     set_body_type(get_body_types()[body_type_index % get_body_types().length]);
-    refreshCanvas(0,"previewCanvas");
+    refreshCanvas(0, "previewCanvas");
   }
 
   draw_next_body_part();
@@ -28,35 +28,44 @@ function next_part() {
   }
 }
 
-function showCurrentGameNotification(){
-  function goToGame(){
-    axios.get(`/api/game/${readCookie('current_game')}/`).then(response=>{
-      if (response.data.is_multiplayer){
-        barba.go('/multigame')
-      }else{
-        barba.go('/game')
+function showCurrentGameNotification() {
+  function goToGame() {
+    axios.get(`/api/game/${readCookie("current_game")}/`).then((response) => {
+      if (response.data.is_multiplayer) {
+        barba.go("/multigame");
+      } else {
+        barba.go("/game");
       }
-    })
+    });
   }
-  if (readCookie('current_game')){
-    let notif = document.createElement('div')
-    notif.innerHTML= 'You are in middle of a game. &ensp;&ensp;&ensp;'
-    notif.classList.add('toastify','on', 'toastify-center', 'max-w-[100%]', 'absolute', 'top-[70px]', 'z-0', 'cursor-auto', 'py-1');
-    let button = document.createElement('button')
-    button.classList.add('bg-green-700','px-5','py-3', 'rounded')
-    button.innerText = 'Continue Playing'
+  if (readCookie("current_game")) {
+    let notif = document.createElement("div");
+    notif.innerHTML = "You are in middle of a game. &ensp;&ensp;&ensp;";
+    notif.classList.add(
+      "toastify",
+      "on",
+      "toastify-center",
+      "max-w-[100%]",
+      "absolute",
+      "top-[70px]",
+      "z-0",
+      "cursor-auto",
+      "py-1"
+    );
+    let button = document.createElement("button");
+    button.classList.add("bg-green-700", "px-5", "py-3", "rounded");
+    button.innerText = "Continue Playing";
     button.onclick = goToGame;
-    notif.append(button)
-    document.getElementsByTagName('main')[0].prepend(notif)
-
+    notif.append(button);
+    document.getElementsByTagName("main")[0].prepend(notif);
   }
 }
 
 export default function loadIndexPage() {
-  refreshCanvas(0,"previewCanvas");
-  setTimeout(()=>{
+  refreshCanvas(0, "previewCanvas");
+  setTimeout(() => {
     showCurrentGameNotification();
-  })
+  });
 
   setTimeout(() => {
     next_part();

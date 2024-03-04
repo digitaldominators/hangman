832ebfff60b14b5283e87d930a6f60b820b3dfc6
Shlomo Markowitz <75799735+smark-1@users.noreply.github.com>
2024-03-03 20:16:37 -0500

    added continue playing notification

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -6,12 +6,15 @@ import {
   draw_next_body_part,
   refreshCanvas,
 } from "./stageCanvas.js";
+import readCookie from './readCookie.js'
+import axios from "axios";
+import barba from "@barba/core";
 let body_type_index = 0;
 function next_part() {
   if (get_current_part_index() >= get_total_parts()) {
     body_type_index += 1;
     set_body_type(get_body_types()[body_type_index % get_body_types().length]);
-    refreshCanvas();
+    refreshCanvas(0,"previewCanvas");
   }
 
   draw_next_body_part();
@@ -24,8 +27,36 @@ function next_part() {
     }, 700);
   }
 }
+
+function showCurrentGameNotification(){
+  function goToGame(){
+    axios.get(`/api/game/${readCookie('current_game')}/`).then(response=>{
+      if (response.data.is_multiplayer){
+        barba.go('/multigame')
+      }else{
+        barba.go('/game')
+      }
+    })
+  }
+  if (readCookie('current_game')){
+    let notif = document.createElement('div')
+    notif.innerHTML= 'You are in middle of a game. &ensp;&ensp;&ensp;'
+    notif.classList.add('toastify','on', 'toastify-center', 'max-w-[100%]', 'absolute', 'top-[70px]', 'z-0', 'cursor-auto', 'py-1');
+    let button = document.createElement('button')
+    button.classList.add('bg-green-700','px-5','py-3', 'rounded')
+    button.innerText = 'Continue Playing'
+    button.onclick = goToGame;
+    notif.append(button)
+    document.getElementsByTagName('main')[0].prepend(notif)
+
+  }
+}
+
 export default function loadIndexPage() {
-  refreshCanvas();
+  refreshCanvas(0,"previewCanvas");
+  setTimeout(()=>{
+    showCurrentGameNotification();
+  })
 
   setTimeout(() => {
     next_part();

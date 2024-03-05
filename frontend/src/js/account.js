ea16264a9913ffa5d2e13238c39203153820db2d
madmurdawg <156305028+madmurdawg@users.noreply.github.com>
2024-03-05 15:16:35 -0500

    added recent game history popup (#113)

--- frontend/src/js/account.js
+++ frontend/src/js/account.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import barba from "@barba/core";
+import { setCookie } from "./readCookie.js";
+
 let password_form;
 let logout_form;
 let password;
 let password2;
 let error_message;
 let showOnLeaderboardSwitch;
+let popup_error_message;
+
 function change_password(e) {
   e.preventDefault();
 
@@ -50,6 +54,89 @@ function loadAccountInfo() {
     }
   });
 }
+
+function displayRecentGames() {
+  axios.get("api/game/").then((response) => {
+    if (Object.keys(response.data).length) {
+      createTable(response.data);
+    } else {
+      document.getElementById("popup_error_message").innerHTML =
+        "You have not played any games yet";
+    }
+  });
+}
+
+function createTable(data) {
+  const table = document.createElement("table");
+  table.setAttribute("id", "game_history_table");
+  const tableHead = document.createElement("thead");
+  const tableBody = document.createElement("tbody");
+  const columnNames = ["Word", "Status", "Score", "Level", "Multiplayer", ""];
+  const gameFields = [
+    "word",
+    "status",
+    "game_score",
+    "level",
+    "is_multiplayer",
+  ];
+
+  // Append the table head and body to the table
+  table.appendChild(tableHead);
+  table.appendChild(tableBody);
+
+  // Create table head
+  let row = tableHead.insertRow();
+  columnNames.forEach((name) => {
+    let column = document.createElement("th");
+    column.textContent = name;
+    if (name === "Level" || name === "Multiplayer") {
+      column.classList.add("hidden", "md:table-cell");
+    }
+
+    row.appendChild(column);
+  });
+
+  // Create table body
+  data.forEach((game) => {
+    let gameSlug = game["game_slug"];
+    let multiplayer = game["is_multiplayer"];
+    let status = game["status"];
+    let row = tableBody.insertRow();
+    gameFields.forEach((column) => {
+      let cell = row.insertCell();
+      cell.textContent = game[column];
+      if (column === "level" || column === "is_multiplayer") {
+        cell.classList.add("hidden", "md:table-cell");
+      }
+    });
+    if (status === "you won" || status === "you lost") {
+      row.insertCell();
+    } else {
+      let cell = row.insertCell();
+      let gameButton = document.createElement("button");
+      gameButton.innerText = "Continue";
+      gameButton.setAttribute("class", "game-history-button");
+      gameButton.addEventListener("click", () => {
+        redirectGame(gameSlug, multiplayer);
+      });
+      cell.appendChild(gameButton);
+    }
+  });
+
+  // Append the table to the HTML document
+  const closeButton = document.getElementById("close_button");
+  document.getElementById("table_container").appendChild(table);
+}
+
+function redirectGame(gameSlug, multiplayer) {
+  setCookie("current_game", gameSlug, 100);
+  if (multiplayer) {
+    barba.go("/multigame");
+  } else {
+    barba.go("/game");
+  }
+}
+
 export default function loadAccountPage() {
   password_form = document.getElementById("password_form");
   logout_form = document.getElementById("logout_form");
@@ -60,6 +147,25 @@ export default function loadAccountPage() {
   showOnLeaderboardSwitch.onchange = updateShowOnLeaderboard;
   password_form.onsubmit = change_password;
   logout_form.onsubmit = logout;
+
+  popup_error_message = document.getElementById("popup_error_message");
+
+  const dialog = document.getElementById("recent_games_dialog");
+  document
+    .getElementById("recent_games_button")
+    .addEventListener("click", () => {
+      displayRecentGames();
+      dialog.showModal();
+    });
+
+  const closeButton = document.getElementById("close_button");
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+    const table = document.getElementById("game_history_table");
+    if (table) {
+      table.remove();
+    }
+  });
 
   loadAccountInfo();
 }
